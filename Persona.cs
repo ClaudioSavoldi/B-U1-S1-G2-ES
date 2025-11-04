876136ad48cd5d8250192eb78dcdc413c4c464cd
Claudio Savoldi <savoldiclaudio02@gmail.com>
2025-11-04 16:02:18 +0100

    Aggiungere i file di progetto.

---
+++ Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_U1_S1_G2_ES
+{
+    public class Persona
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Persona(string name, string lastname, int age)
+        {
+            Name = name;
+            LastName = lastname;
+            Age = age;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public string GetLastname()
+        {
+            return LastName;
+        }
+        public int GetAge()
+        {
+            return Age;
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine($"il nome è {Name}, il cognome è {LastName} e l`età è {Age}");
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using B_U1_S1_G2_ES;
+
+var persona1 = new Persona("Claudio", "Savoldi", 23);
+
+Console.WriteLine(persona1.GetName());
+Console.WriteLine(persona1.GetLastname());
+Console.WriteLine(persona1.GetAge());
+
+persona1.GetDetails();
